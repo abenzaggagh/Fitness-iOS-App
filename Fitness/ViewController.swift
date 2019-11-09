0b385e3e9a08f59ec87f998f3694f88fdc5b5fb9
Amine <4min3b3n@gmail.com>
2019-11-09 13:39:10 +0100

    Initial Commit

---
+++ Fitness/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fitness
+//
+//  Created by Amine BEN ZAGGAGH on 11/9/19.
+//  Copyright © 2019 Amine BEN ZAGGAGH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
