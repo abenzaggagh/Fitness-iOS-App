a2bd85e3e536e4ab237f095fddbdcc84c3b85ef4
Amine <4min3b3n@gmail.com>
2019-11-10 16:42:01 +0100

    Tab Bar Working
    TODO: Make every bar tab item as navigation

---
+++ Fitness/Authentication/AuthenticationViewController.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+
+class GoalViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    @IBAction func loseWeight(_ sender: UIButton) {
+    
+    }
+    
+}
---
+++ Fitness/Summary/SummaryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  Fitness
+//
+//  Created by Amine BEN ZAGGAGH on 11/10/19.
+//  Copyright © 2019 Amine BEN ZAGGAGH. All rights reserved.
+//
+
+import UIKit
+
+class SummaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
